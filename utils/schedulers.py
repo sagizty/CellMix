592cb328325dafabb8529907df6973c00b6b0bb0
Tianyi Zhang <50575108+sagizty@users.noreply.github.com>
2024-06-26 20:33:11 +0800

    Update details in schedulers.py

--- utils/schedulers.py
+++ utils/schedulers.py
@@ -1,8 +1,11 @@
 """
-Schedulers   Script  ver： May 19th 12:00
+Schedulers   Script  ver： Jun 26th 20:00
+
+puzzle_patch_scheduler is used to arrange patch size for multi-scale learning
+
+ref
 lr_scheduler from MAE code.
 https://github.com/facebookresearch/mae
-puzzle_patch_scheduler is used to arrange patch size for multi-scale learning
 """
 
 import math
@@ -63,7 +66,7 @@ class patch_scheduler:
     """
 
     def __init__(self, total_epoches=200, warmup_epochs=20, edge_size=384, basic_patch=16, strategy=None,
-                 threshold=4.0, reducing_factor=0.933, fix_patch_size=None, patch_size_jump=None):
+                 loop_round_epoch=4, reducing_factor=0.933, fix_patch_size=None, patch_size_jump=None):
         super().__init__()
 
         self.strategy = strategy
@@ -71,10 +74,10 @@ class patch_scheduler:
         self.total_epoches = total_epoches
         self.warmup_epochs = warmup_epochs
 
-        # automaticly build legal patch list, from small to big size
+        # automatically build legal patch list, from small to big size
         self.patch_list = defactor(factor(edge_size), basic_patch)
 
-        self.threshold = threshold
+        self.loop_round_epoch = loop_round_epoch
         self.reducing_factor = reducing_factor
         self.fix_patch_size = fix_patch_size
 
@@ -92,7 +95,7 @@ class patch_scheduler:
             temp_list = [self.patch_list[0]]
             temp_list.extend(jump_patch_list)
             self.patch_list = temp_list
-        else:
+        else:  # all
             pass
 
         if self.strategy in ['reverse', 'loss_back', 'loss_hold']:  # start from big(easy) to samll(complex)
@@ -104,11 +107,9 @@ class patch_scheduler:
         else:
             print('patch_list:', self.patch_list)
 
-        # self.loss_log ?
-
     def __call__(self, epoch, loss=0.0):
         # Designed for flexable ablations
-        if self.strategy == 'linear' or self.strategy == 'reverse':  # reverse from big size to small
+        if self.strategy == 'linear' or self.strategy == 'reverse':  # single round
             if epoch < self.warmup_epochs:  # warmup
                 puzzle_patch_size = 32  # fixed size for warmup
             else:
@@ -116,12 +117,14 @@ class patch_scheduler:
                                                             / (self.total_epoches - self.warmup_epochs)
                                                             * len(self.patch_list)), len(self.patch_list) - 1)]
 
-        elif self.strategy == 'loop':
-            # looply change the patch size, after [group_size] epoches we change once
-            group_size = 3
+        # reverse from big size to small, multiple rounds
+        elif self.strategy == 'loop' or self.strategy == 'loss_back' or self.strategy == 'loss_hold':
+            # notice loss-drive(loss_back/loss_hold) is not applied to patch, it is more meaningful for ratio:
+            # looply change the patch size, after [group_size] epochs we change once
+            group_size = int(self.loop_round_epoch)
 
             if epoch < self.warmup_epochs:
-                puzzle_patch_size = 32  # in warm up epoches, fixed patch size at 32 fixme exploring
+                puzzle_patch_size = 32  # in warm up epoches, fixed patch size at 32
             else:
                 group_idx = (epoch - self.warmup_epochs) % (len(self.patch_list) * group_size)
                 puzzle_patch_size = self.patch_list[int(group_idx / group_size)]
@@ -129,72 +132,28 @@ class patch_scheduler:
         elif self.strategy == 'random':  # random size strategy
             puzzle_patch_size = random.choice(self.patch_list)
 
-        elif self.strategy == 'loss_back':
-            if epoch < self.warmup_epochs:  # for warmup
-                puzzle_patch_size = 32  # in warm-up we use the fix size
-            else:
-                if loss == 0.0:
-                    puzzle_patch_size = self.patch_list[min(int((epoch - self.warmup_epochs)
-                                                                / (self.total_epoches - self.warmup_epochs)
-                                                                * len(self.patch_list)), len(self.patch_list) - 1)]
-
-                elif loss < self.threshold:
-                    puzzle_patch_size = self.patch_list[min(max(int((epoch - self.warmup_epochs)
-                                                                    / (self.total_epoches - self.warmup_epochs)
-                                                                    * len(self.patch_list)) + 1, 0),
-                                                            len(self.patch_list) - 1)]
-                    self.threshold *= self.reducing_factor
-                else:
-                    puzzle_patch_size = self.patch_list[min(max(int((epoch - self.warmup_epochs)
-                                                                    / (self.total_epoches - self.warmup_epochs)
-                                                                    * len(self.patch_list)) - 1, 0),
-                                                            len(self.patch_list) - 1)]
-
-        elif self.strategy == 'loss_hold':
-            if epoch < self.warmup_epochs:  # for warmup
-                puzzle_patch_size = 32  # in warm-up we use the fix size
-            else:
-                if loss == 0.0:
-                    puzzle_patch_size = self.patch_list[min(int((epoch - self.warmup_epochs)
-                                                                / (self.total_epoches - self.warmup_epochs)
-                                                                * len(self.patch_list)), len(self.patch_list) - 1)]
-
-                elif loss < self.threshold:
-                    puzzle_patch_size = self.patch_list[min(max(int((epoch - self.warmup_epochs)
-                                                                    / (self.total_epoches - self.warmup_epochs)
-                                                                    * len(self.patch_list)) + 1, 0),
-                                                            len(self.patch_list) - 1)]
-                    self.threshold *= self.reducing_factor
-                else:
-                    puzzle_patch_size = self.patch_list[min(max(int((epoch - self.warmup_epochs)
-                                                                    / (self.total_epoches - self.warmup_epochs)
-                                                                    * len(self.patch_list)), 0),
-                                                            len(self.patch_list) - 1)]
-
         else:
-            # if self.strategy is None or 'fixed' or 'ratio-decay'
+            # if self.strategy is None or 'fixed' or using other ablations fixing the patch size,
+            # such as 'ratio-decay'(in PuzzleTuning)
             puzzle_patch_size = self.fix_patch_size or self.patch_list[0]  # basic_patch
 
         return puzzle_patch_size
 
 
-'''
-scheduler = puzzle_patch_scheduler(strategy='reverse')
-epoch = 182
-puzzle_patch_size = scheduler(epoch)
-print(puzzle_patch_size)
-'''
-
-
 class ratio_scheduler:
     """
-        this is used to drive the fix position ratio by loss and epoch
-        the ratio is control by ratio_floor_factor=0.5, upper_limit=0.9, lower_limit=0.2
-    """
-    def __init__(self, total_epoches=200, warmup_epochs=20, basic_ratio=0.25, strategy=None, fix_position_ratio=None,
-                 threshold=4.0, loss_reducing_factor=0.933, ratio_floor_factor=0.5, upper_limit=0.9, lower_limit=0.2):
+    This is used to drive the fix position ratio by loss and epoch
+    the ratio is control by ratio_floor_factor=0.5, upper_limit=0.9, lower_limit=0.2 in default
 
-        # fixme basic_ratio and fix_position_ratio(when stage is fixed) is a bit conflicting, not good enough
+        In CellMix paper (Nov 2022),
+        'loss_back' and 'loss_hold' are designed
+    """
+
+    def __init__(self, total_epoches=200, warmup_epochs=20, basic_ratio=0.25, strategy=None, fix_position_ratio=None,
+                 loop_round_epoch=4, loss_reducing_factor=0.933, ratio_floor_factor=0.5, upper_limit=0.9,
+                 lower_limit=0.2):
+
+        # fixme basic_ratio and fix_position_ratio(when stage is fixed) maybe a bit confusing
         super().__init__()
         self.strategy = strategy
 
@@ -202,8 +161,8 @@ class ratio_scheduler:
         self.warmup_epochs = warmup_epochs
 
         self.basic_ratio = basic_ratio
-
-        self.threshold = threshold
+        # this loop_round_epoch is designed to record the epoch bandwidth for epochs at same patch size
+        self.loop_round_epoch = loop_round_epoch
         self.loss_reducing_factor = loss_reducing_factor
 
         self.fix_position_ratio = fix_position_ratio
@@ -212,10 +171,99 @@ class ratio_scheduler:
         self.lower_limit = lower_limit
         self.ratio_floor_factor = ratio_floor_factor
 
+        self.loss_log = []
+        self.kick_off = True  # starting
+        self.previous_ratio = basic_ratio  # starting
+
     def __call__(self, epoch, loss=0.0):
-        if self.strategy == 'ratio-decay' or self.strategy == 'decay':
+        if self.strategy == 'fixed':
+            fix_position_ratio = self.fix_position_ratio or self.basic_ratio
+
+        elif self.strategy == 'loss_back':
+            # looply change the ratio within the set of patch size -> after [group_size] epochs we change once
+            group_size = int(self.loop_round_epoch)
+            max_ratio = min(3 * self.basic_ratio, self.upper_limit)
+            min_ratio = max(self.basic_ratio * self.ratio_floor_factor, self.lower_limit)
+
+            if epoch < self.warmup_epochs:  # for warmup
+                fix_position_ratio = self.basic_ratio  # in warm-up, we use the fix ratio
+
+            else:
+                group_idx = (epoch - self.warmup_epochs) % group_size
+
+                if group_idx == 0:
+                    Reduced_moving_average = sum(self.loss_log) / len(self.loss_log) * self.loss_reducing_factor
+
+                    if loss == 0.0 or self.kick_off:  # start without warm-up or right after warm-up
+                        self.kick_off = False  # turn off as its finished
+                        fix_position_ratio = min(max(((self.total_epoches - self.warmup_epochs)
+                                                      - (epoch - self.warmup_epochs)) /
+                                                     (self.total_epoches - self.warmup_epochs)
+                                                     * max_ratio, min_ratio), max_ratio)
+                    elif loss < Reduced_moving_average:
+                        # loss is decreasing nicely, we increase the difficulty
+                        fix_position_ratio = min(max(((self.total_epoches - self.warmup_epochs)
+                                                      - (epoch - self.warmup_epochs)) /
+                                                     (self.total_epoches - self.warmup_epochs)
+                                                     * max_ratio * 0.9, min_ratio), max_ratio)
+                    else:
+                        # loss is not decreasing nicely, we reduce the status difficulty
+                        fix_position_ratio = min(max(((self.total_epoches - self.warmup_epochs)
+                                                      - (epoch - self.warmup_epochs)) /
+                                                     (self.total_epoches - self.warmup_epochs)
+                                                     * max_ratio * 1.1, min_ratio), max_ratio)
+                    # empty the loss log in this patch size round
+                    self.loss_log = []
+                else:
+                    fix_position_ratio = self.previous_ratio
+
+            # update
+            self.loss_log.append(float(loss))
+            self.previous_ratio = fix_position_ratio
+
+        elif self.strategy == 'loss_hold':
+            # looply change the ratio within the set of patch size -> after [group_size] epochs we change once
+            group_size = int(self.loop_round_epoch)
+            max_ratio = min(3 * self.basic_ratio, self.upper_limit)
+            min_ratio = max(self.basic_ratio * self.ratio_floor_factor, self.lower_limit)
+
+            if epoch < self.warmup_epochs:  # for warmup
+                fix_position_ratio = self.basic_ratio  # in warm-up we use the fix ratio
+            else:
+                group_idx = (epoch - self.warmup_epochs) % group_size
+                if group_idx == 0:
+                    Reduced_moving_average = sum(self.loss_log) / len(self.loss_log) * self.loss_reducing_factor
+
+                    if loss == 0.0:  # start without warmup
+                        fix_position_ratio = min(max(((self.total_epoches - self.warmup_epochs)
+                                                      - (epoch - self.warmup_epochs)) /
+                                                     (self.total_epoches - self.warmup_epochs)
+                                                     * max_ratio, min_ratio), max_ratio)
+                    elif loss < Reduced_moving_average:
+                        # loss is decreasing nicely, we increase the difficulty
+                        fix_position_ratio = min(max(((self.total_epoches - self.warmup_epochs)
+                                                      - (epoch - self.warmup_epochs)) /
+                                                     (self.total_epoches - self.warmup_epochs)
+                                                     * max_ratio * 0.9, min_ratio), max_ratio)
+                    else:
+                        # loss is not decreasing nicely, we hold the status difficulty
+                        fix_position_ratio = min(max(((self.total_epoches - self.warmup_epochs)
+                                                      - (epoch - self.warmup_epochs)) /
+                                                     (self.total_epoches - self.warmup_epochs)
+                                                     * max_ratio, min_ratio), max_ratio)
+                    # empty the loss log in this patch size round
+                    self.loss_log = []
+                else:
+                    fix_position_ratio = self.previous_ratio
+
+            # update
+            self.loss_log.append(float(loss))
+            self.previous_ratio = fix_position_ratio
+
+        else:  # self.strategy == 'ratio-decay' or self.strategy == 'decay'
             if epoch < self.warmup_epochs:  # for warmup
                 fix_position_ratio = self.basic_ratio  # fixed
+                self.previous_loss = loss
             else:
                 max_ratio = min(3 * self.basic_ratio, self.upper_limit)  # upper-limit of 0.9
                 min_ratio = max(self.basic_ratio * self.ratio_floor_factor, self.lower_limit)
@@ -225,66 +273,4 @@ class ratio_scheduler:
                                              (self.total_epoches - self.warmup_epochs)
                                              * max_ratio, min_ratio), max_ratio)
 
-        elif self.strategy == 'loss_back':
-
-            if epoch < self.warmup_epochs:  # for warmup
-                fix_position_ratio = self.basic_ratio  # in warm-up we use the fix ratio
-
-            else:
-                max_ratio = min(3 * self.basic_ratio, self.upper_limit)
-                min_ratio = max(self.basic_ratio * self.ratio_floor_factor, self.lower_limit)
-                if loss == 0.0:
-                    fix_position_ratio = min(max(((self.total_epoches - self.warmup_epochs)
-                                                  - (epoch - self.warmup_epochs)) /
-                                                 (self.total_epoches - self.warmup_epochs)
-                                                 * max_ratio, min_ratio), max_ratio)
-                elif loss < self.threshold:
-                    fix_position_ratio = min(max(((self.total_epoches - self.warmup_epochs)
-                                                  - (epoch - self.warmup_epochs)) /
-                                                 (self.total_epoches - self.warmup_epochs)
-                                                 * max_ratio * 0.9, min_ratio), max_ratio)
-                    self.threshold *= self.loss_reducing_factor
-                else:
-                    fix_position_ratio = min(max(((self.total_epoches - self.warmup_epochs)
-                                                  - (epoch - self.warmup_epochs)) /
-                                                 (self.total_epoches - self.warmup_epochs)
-                                                 * max_ratio * 1.1, min_ratio), max_ratio)
-
-        elif self.strategy == 'loss_hold':
-
-            if epoch < self.warmup_epochs:  # for warmup
-                fix_position_ratio = self.basic_ratio  # in warm-up we use the fix ratio
-
-            else:
-                max_ratio = min(3 * self.basic_ratio, self.upper_limit)
-                min_ratio = max(self.basic_ratio * self.ratio_floor_factor, self.lower_limit)
-
-                if loss == 0.0:
-                    fix_position_ratio = min(max(((self.total_epoches - self.warmup_epochs)
-                                                  - (epoch - self.warmup_epochs)) /
-                                                 (self.total_epoches - self.warmup_epochs)
-                                                 * max_ratio, min_ratio), max_ratio)
-                elif loss < self.threshold:
-                    fix_position_ratio = min(max(((self.total_epoches - self.warmup_epochs)
-                                                  - (epoch - self.warmup_epochs)) /
-                                                 (self.total_epoches - self.warmup_epochs)
-                                                 * max_ratio * 0.9, min_ratio), max_ratio)
-                    self.threshold *= self.loss_reducing_factor
-                else:
-                    fix_position_ratio = min(max(((self.total_epoches - self.warmup_epochs)
-                                                  - (epoch - self.warmup_epochs)) /
-                                                 (self.total_epoches - self.warmup_epochs)
-                                                 * max_ratio, min_ratio), max_ratio)
-
-        else:  # basic_ratio
-            fix_position_ratio = self.fix_position_ratio or self.basic_ratio
-
         return fix_position_ratio
-
-
-'''
-scheduler = puzzle_fix_position_ratio_scheduler(strategy='reverse')
-epoch = 102
-fix_position_ratio = scheduler(epoch)
-print(fix_position_ratio)
-'''
